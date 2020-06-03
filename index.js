0b32b988e27ea55974eb09788ee90bf71bef475c
Douglas <douglas@dobuilding.co.uk>
2020-06-03 11:47:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(arr, event) {
+    let array = []
+    for (let i = 0; i < arr.length;i++){
+        array.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+return array;
+
+}
+
+
+function countDown(times) {
+while (times >= 0){
+    console.log(times);
+    times -= 1;
+}
+
+
+}
